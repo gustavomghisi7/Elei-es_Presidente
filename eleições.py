65b5b4c9e27c9e9c099ba8ae48fad07dc07e74f8
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2022-10-03 17:15:12 -0300

    ajuste

--- eleições.py
+++ eleições.py
@@ -18,7 +18,7 @@ for inf in json_dados['cand']:
     if inf['seq'] == '1' or inf['seq'] == '2' or inf['seq'] == '3' or inf['seq'] == '4' or inf['seq'] == '5' :
         candidato.append(inf['nm']), partido.append(inf['n']), porcetagem.append(inf['pvap']), votos.append(inf['vap'])
 
-pres_eleicao = pd.DataFrame(list(zip(candidato, partido, porcetagem, votos, )), columns = [
+pres_eleicao = pd.DataFrame(list(zip(candidato, partido, porcetagem, votos)), columns = [
     'Candidato', 'Partido', 'Porcetagem', 'Num de Votos'
 ])
 
